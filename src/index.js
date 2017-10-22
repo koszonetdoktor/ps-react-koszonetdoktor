c0fc3bb456b546cc67538bd31e055c030b5be220
Gergo <gyikarcu@gmail.com>
2017-10-22 12:53:40 +0200

    documentation part added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Docs from './docs/Docs';
+import '../node_modules/highlight.js/styles/ocean.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<Docs />, document.getElementById('root'));
+
